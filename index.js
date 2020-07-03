090faa588bfbe08015daded8a7d41ed2125a5ae8
Edward Gonzalez <edward.gonzalez9708@gmail.com>
2020-07-02 23:23:17 -0400

    Added a timer function to display the loader icon a second longer before display dog images onto the screen.

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const DOG_URL = "https://dog.ceo/api/breeds/image/random"
-const DOG_BREEDS_URL =  "https://dog.ceo/api/breeds/list/all"
+//const DOG_URL = "https://dog.ceo/api/breeds/image/random"
+const ALL_BREEDS_URL =  "https://dog.ceo/api/breeds/list/all"
+const BREEDS_URL = "https://dog.ceo/api/breed"
 
 const doggos = document.querySelector(".doggos");
+const loader = document.querySelector(".loader");
 
 function addNewDoggo(e) {
     e.preventDefault();
-    const promise = fetch(DOG_URL);
+    // const promise = fetch(DOG_URL);
+    const sel = document.querySelector('#breeds-list');
+    const option = sel.options[sel.selectedIndex];
+    let breedPath = "";
+
+    option.value.split("-")
+    .forEach((s) => {
+        if (!breedPath) {
+            breedPath = s;
+        } else {
+            breedPath += `/${s}`;
+        }     
+    });
+
+    const promise = fetch(`${BREEDS_URL}/${breedPath}/images/random`);
 
     promise
     .then(function(response) {
         const processingPromise = response.json();
+        doggos.style.display = "none";
+        loader.style.display = "initial";
         return processingPromise;
     })
     .then(function(processedResponse) {
         const img = document.createElement("img");
         img.src = processedResponse.message;
-        img.alt = "Cute doggo";
+        img.alt = option.innerText;
         doggos.appendChild(img);
+        setTimeout(() => {
+            doggos.style.display = "flex";
+            loader.style.display = "none";
+        }, 1000);
     });
 }
 
 document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
 
 function fetchBreeds() {
-    const promise = fetch(DOG_BREEDS_URL);
+    const promise = fetch(ALL_BREEDS_URL);
 
     promise
     .then(function(response) {
@@ -39,13 +61,13 @@ function fetchBreeds() {
                 for (index in processedResponse.message[breed]) {
                     const option = document.createElement("option");
                     const breedType = processedResponse.message[breed][index];
-                    option.val = `${breed}-${breedType}`;
+                    option.value = `${breed}-${breedType}`;
                     option.innerText = `${breedType[0].toUpperCase()}${breedType.slice(1)} ${breed[0].toUpperCase()}${breed.slice(1)}`;
                     breedsList.push(option);
                 }
             } else {
                 const option = document.createElement("option");
-                option.val = breed;
+                option.value = breed;
                 option.innerText = `${breed[0].toUpperCase()}${breed.slice(1)}`
                 breedsList.push(option);
             }
